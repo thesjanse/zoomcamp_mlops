932caf85db8d15b2d2f32a7e62abe234aded9ac2
Alexander Tretyak <alexander.mejinski@gmail.com>
2023-07-16 16:53:24 +0300

    feature: last questions for week6

--- week6/jupyter/scripts/batch.py
+++ week6/jupyter/scripts/batch.py
@@ -1,31 +1,59 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import sys
 import pickle
 import pandas as pd
+import os
 
 
-def read_data(filename: str, categorical: list) -> pd.DataFrame:
-    """Read data and prepare it for the forecasting."""
+storage_options = {
+    'client_kwargs': {
+        'endpoint_url': os.getenv('AWS_ENDPOINT_URL')
+    }
+}
+
+
+def read_data(filename: str) -> pd.DataFrame:
+    """Read parquet data from filename."""
     df = pd.read_parquet(filename)
-    
+    return df
+
+
+def save_data(df: pd.DataFrame, output_path: str) -> None:
+    df.to_parquet(
+        output_path,
+        engine='pyarrow',
+        compression=None,
+        index=False,
+    )
+
+
+def get_input_path(year, month: str):
+    default_input_pattern = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    return input_pattern.format(year=year, month=month)
+
+
+def get_output_path(year, month):
+    default_output_pattern = './data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
+    return output_pattern.format(year=year, month=month)
+
+
+def prepare_data(df: pd.DataFrame, categorical: list) -> pd.DataFrame:
+    """Transform dataframe and prepare for the forecasting"""
     df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
-
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
-
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
-
     return df
 
 
 def main(year, month: int) -> None:
-    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-    output_file = f'./data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    input_file = get_input_path(year=year, month=month)
+    output_file = get_output_path(year=year, month=month)
     categorical = ['PULocationID', 'DOLocationID']
 
-    df = read_data(filename=input_file, categorical=categorical)
+    df = read_data(filename=input_file)
+    df = prepare_data(df=df, categorical=categorical)
     df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
 
     with open('./model/model.bin', 'rb') as f_in:
